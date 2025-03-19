4bc12d97927d1f6192a35398965f9653984a97ef
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-03-19 23:34:15 +0530

    update code

--- src/app.js
+++ src/app.js
@@ -4,7 +4,13 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
