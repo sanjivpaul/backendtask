af40d1359e15d904fd0c1f6af2f08bf4871aaef4
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-03-19 22:52:37 +0530

    update code

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const connectDatabase = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+      // `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+      `${process.env.MONGODB_URI}`
     );
 
     // const connectionInstance = await mongoose.connect(
