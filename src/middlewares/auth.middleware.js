1c4c33f0f316d8af5d9a300d8d80b13689abd572
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-03-20 17:17:00 +0530

    update controller and add auth on router

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { User } from "../models/user.model.js";
+import { User } from "../models/user/user.model.js";
 
 export const verifyJwt = async (req, res, next) => {
   try {
--- src/routes/task/task.routes.js
+++ src/routes/task/task.routes.js
@@ -6,9 +6,10 @@ import {
   getTaskById,
   updateTask,
 } from "../../controllers/task/task.controller.js";
+import { verifyJwt } from "../../middlewares/auth.middleware.js";
 
 const router = Router();
-
+router.use(verifyJwt);
 router.route("/").post(createTask);
 router.route("/").get(getAllTask);
 router.route("/:id").get(getTaskById);
